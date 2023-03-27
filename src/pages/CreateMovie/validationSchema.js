8249d850e9d8a720af01cb5112106814bc913abc
AlexHalynskyiTest <alexhalynskyi@gmail.com>
2023-03-27 19:59:44 +0300

    Fixed validation of title and actors

--- src/pages/CreateMovie/validationSchema.js
+++ src/pages/CreateMovie/validationSchema.js
@@ -1,7 +1,9 @@
 import * as Yup from 'yup';
 
 const movieSchema = Yup.object().shape({
-  title: Yup.string().required('Required'),
+  title: Yup.string()
+    .required('Required')
+    .test('empty', 'Empty field is not allowed', val => val?.split(' ').filter(Boolean).length > 0),
   year: Yup.number()
     .min(1880, 'Minimum 1880 year')
     .max(2030, 'Maximum 2030 year')
@@ -11,7 +13,12 @@ const movieSchema = Yup.object().shape({
     .min(1, 'Must include minimum 1 actor')
     .required('Must have actors')
     .of(Yup.string()
-      .test('len', 'Actor name must contain at least 1 letter', val => val?.length > 0)),
+      .matches(
+        /^[a-zA-Z\s,-]*$/,
+        "Only letters, comma and hyphen allowed in actor name"
+      )
+      .test('len', 'Actor name must contain at least 1 letter', val => val?.length > 0)
+      .test('empty', 'Empty field is not allowed', val => val?.split(' ').filter(Boolean).length > 0)),
 });
 
 export default movieSchema
